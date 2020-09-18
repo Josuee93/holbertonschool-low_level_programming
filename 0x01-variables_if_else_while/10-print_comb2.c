8a788a32b35fb268fc937a5549e326faac587ec6
Josuee93 <joxuecont@gmail.com>
2020-09-18 20:40:17 +0000

    print_comb2

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-  int i;
-  for (i = 0; i < 100; i++)
-    {
-      putchar((i / 10) + '0');
-      putchar((i % 10) + '0');
-      if (i != 99)
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
-  return (0);
+int i;
+for (i = 0; i < 100; i++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+if (i != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
