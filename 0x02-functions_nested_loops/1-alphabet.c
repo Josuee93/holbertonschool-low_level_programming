1f44ac474aece5c2353de730c3412e9886259ce8
Josuee93 <joxuecont@gmail.com>
2020-09-22 15:20:10 +0000

    Functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/*
+ *Function that prints the alphabet
+ *in lower case
+*/
+void print_alphabet(void)
+{
+char i;
+for(i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+
