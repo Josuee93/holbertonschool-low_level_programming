f5684928a8c626238608789c9ea655a03ce6de22
Josuee93 <joxuecont@gmail.com>
2020-09-22 14:23:34 +0000

    Functions

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _putchar(char);
+int main(void)
+{
+char string[] = "Holberton";
+int i;
+for (i = 0; i <= 8; i++)
+{
+_putchar(string[i]);
+}
+_putchar('\n');
+return (0);
+}
