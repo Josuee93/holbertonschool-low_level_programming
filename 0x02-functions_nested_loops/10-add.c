9d8f8f1085ea4068f9b78426d7e37d4596237ee8
Josuee93 <joxuecont@gmail.com>
2020-09-23 02:45:38 +0000

    Functions

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-
+/**
+ *add - Adds two integers and returns the result.
+ *@a: The first value to check.
+ *@b: The second value to check.
+ *
+ *Return: the result of two integers.
+ */
 int add(int a, int b)
 {
 	return (a + b);
