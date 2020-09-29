eed8bd537cbcd47c4399717bb5eb3db03ab728a6
Josuee93 <joxuecont@gmail.com>
2020-09-29 13:55:26 +0000

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,8 +2,18 @@
 
 void rev_string(char *s)
 {
-	int length, i;
+	int length, i, last_pos;
 	char temp[] = "";
 
-	
+	while (s[length] != '\0')
+		length++;
+
+	last_pos = length - 1;
+
+	for (i = 0; i < length / 2; i++)
+	{
+		*temp = s[i];
+		s[i] = s[last_pos - i];
+		s[last_pos - i] = *temp;
+	}
 }
